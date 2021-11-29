62af5d672ce44be166a98a8906c2c2bf24b5a83e
McGreggor D Kennison <82989377+GregieBoi@users.noreply.github.com>
2021-11-29 00:13:57 -0500

    Add files via upload

---
+++ ch03-ex08.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	
+        int val = 0; // initialize val as 0
+	    cout << "Please enter an integer: "; // prompt for input
+	    cin >> val; // set val to input
+	    string res = "even"; // set res to even
+	    if (val % 2) res = "odd"; // check to see if num is odd
+
+	    cout << "The value " << val << " is an " << res << " number\n"; // print output
+    
+}
